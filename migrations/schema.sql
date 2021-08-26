cad4f181977e72bdbfa65a5373218648df8baf95
Danilo Pereira <danilo4web@gmail.com>
2021-08-26 20:01:58 -0300

    add tables order and order_item

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,11 +1,37 @@
 create schema ccca;
 
-create table ccca.item (
-    id serial,
+create table ccca.item(
+    id          serial,
     description text,
-    price numeric,
-    height integer,
-    width integer,
-    length integer,
-    weight integer
+    price       numeric,
+    height      integer,
+    width       integer,
+    length      integer,
+    weight      integer
+);
+
+create table ccca.coupon(
+    code        text,
+    percentage  numeric,
+    expire_date timestamp,
+    primary key (code)
+);
+
+create table ccca.order(
+    id          serial,
+    coupon_code text,
+    code        text,
+    cpf         text,
+    issue_date  timestamp,
+    freight     numeric,
+    serial      integer,
+    primary key (id)
+);
+
+create table ccca.order_item(
+    id_order integer,
+    id_item  integer,
+    price    numeric,
+    quantity integer,
+    primary key (id_order, id_item)
 );
